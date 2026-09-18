18256edd945b5bff61761c71d2c22d6b5d1c3769
Nazeeha Farvin <nazeehafarvin8@gmail.com>
2026-09-18 13:40:31 +0530

    Update hp.java

--- hp.java
+++ hp.java
@@ -1,12 +1,16 @@
-public class ProfileCheck {
-    public static void main(String[] args) {
-        String username = null; 
+def calculate_average(numbers):
+    total = 0
+    for num in numbers:
+        total += num
+    
+    # Bug: Division by zero if the list is empty
+    return total / len(numbers)
 
-        // Bug: Calling a method on a null reference
-        if (username.equals("admin")) {
-            System.out.println("Access granted.");
-        } else {
-            System.out.println("Access denied.");
-        }
-    }
-}
+# Bug: Modifying a list while iterating over it
+data = [10, 20, 30, 40, 50]
+for item in data:
+    if item > 20:
+        data.remove(item)
+
+print("Remaining data:", data)
+print("Average:", calculate_average(data))
